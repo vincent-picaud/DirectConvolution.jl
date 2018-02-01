43730cd7da088cbbcc55332863650278d390d247
autodocs <autodocs>
2018-02-01 10:40:23 +0000

    build based on 6b22e1b

---
+++ latest/search_index.js
@@ -0,0 +1,91 @@
+var documenterSearchIndex = {"docs": [
+
+{
+    "location": "index.html#",
+    "page": "Home",
+    "title": "Home",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "index.html#Direct-Convolution-1",
+    "page": "Home",
+    "title": "Direct Convolution",
+    "category": "section",
+    "text": "For small kernels this approach is more efficient than FFTgammak=sumlimits_iinOmega^alphaalphaibetak+lambda itext with lambdainmathbbZ^*CAVEAT: under construction"
+},
+
+{
+    "location": "index.html#Demos-1",
+    "page": "Home",
+    "title": "Demos",
+    "category": "section",
+    "text": "Pages = [\n    \"demos.md\"]\nDepth = 1"
+},
+
+{
+    "location": "index.html#DirectConvolution.direct_conv-Union{Tuple{T}, Tuple{AbstractArray{T,1},Int64,Int64,AbstractArray{T,1},Symbol,Symbol}} where T",
+    "page": "Home",
+    "title": "DirectConvolution.direct_conv",
+    "category": "Method",
+    "text": "direct_conv(...)\n\nCompute convolution.\n\nHere's some inline maths: sqrtn1 + x + x^2 + ldots.\n\nHere's an equation:\n\nfracnk(n - k) = binomnk\n\nThis is the binomial coefficient.\n\nReturn γ, a created vector of length identical to β one.\n\n\n\n"
+},
+
+{
+    "location": "index.html#Autodoc...-1",
+    "page": "Home",
+    "title": "Autodoc...",
+    "category": "section",
+    "text": "Modules = [DirectConvolution]\nOrder   = [:type, :function]"
+},
+
+{
+    "location": "index.html#Benchmarks-1",
+    "page": "Home",
+    "title": "Benchmarks",
+    "category": "section",
+    "text": "Pages = [\n    \"benchmarks.md\"]"
+},
+
+{
+    "location": "index.html#Index-1",
+    "page": "Home",
+    "title": "Index",
+    "category": "section",
+    "text": ""
+},
+
+{
+    "location": "demos.html#",
+    "page": "Demos",
+    "title": "Demos",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "demos.html#Demos-1",
+    "page": "Demos",
+    "title": "Demos",
+    "category": "section",
+    "text": "TODO -> SG filter, wavelets ?"
+},
+
+{
+    "location": "benchmarks.html#",
+    "page": "Benchmarks",
+    "title": "Benchmarks",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "benchmarks.html#Benchmarks-1",
+    "page": "Benchmarks",
+    "title": "Benchmarks",
+    "category": "section",
+    "text": "TODO -> compare FFT and directConvolution"
+},
+
+]}
