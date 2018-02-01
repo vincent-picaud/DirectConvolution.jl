5b96ec3586744cf8975864916ee8b78e6d653e76
autodocs <autodocs>
2018-02-01 18:35:35 +0000

    build based on 71482b9

--- latest/search_index.js
+++ latest/search_index.js
@@ -17,19 +17,27 @@ var documenterSearchIndex = {"docs": [
 },
 
 {
-    "location": "index.html#Usage-examples-1",
+    "location": "index.html#Basic-usage-examples-1",
     "page": "Home",
-    "title": "Usage examples",
+    "title": "Basic usage examples",
     "category": "section",
-    "text": "Let's start with a basic example, with zero padding boundary extension. This example shows the role of the α_offset parameter.using DirectConvolution\nα=Float64[0,1,0];\nβ=collect(Float64,1:6);\nγ1=directConv(α,0,1,β,:ZeroPadding,:ZeroPadding); # α_offset = 0\nγ2=directConv(α,1,1,β,:ZeroPadding,:ZeroPadding); # α_offset = 1\nprintln(\"Filter coefficients\") # hide\nprintln(\"α = $(α)\") # hide\nprintln(\"Computation with α_offset=0 (observe the signal shift)\") # hide\nprintln(\"β  = $(β)\") # hide\nprintln(\"γ1 = $(γ1)\") # hide\nprintln(\"Computation with α_offset=1 (observe the phase is corrected)\") # hide\nprintln(\"β  = $(β)\") # hide\nprintln(\"γ2 = $(γ2)\") # hide"
+    "text": ""
 },
 
 {
-    "location": "index.html#Comparison-with-FFT-1",
+    "location": "index.html#Performance-1",
     "page": "Home",
-    "title": "Comparison with FFT",
+    "title": "Performance",
     "category": "section",
-    "text": "TODOCAVEAT: under construction"
+    "text": "Typically wavelet transform involve small filters, of size 5 for instance. We want to compare directConv to native Julia conv function.First one must check that the two methods return the same result. Note that conv function return a vector γ of length |α|+|β|-1 whereas our function respect initial β bounds, hence the returned γ vector has same size as input β.using DirectConvolution, BenchmarkTools\nα=rand(5);\nβ=rand(1000);\nr1=conv(α,β);\nr2=directConv(α,0,-1,β,:ZeroPadding,:ZeroPadding);\nprintln(\"Result comparison $(r1[1:1000] ≈ r2)\")\nprintln(\"Julia conv()\")\n@benchmark conv(α,β);\nprintln(\"This directConv()\")\n@benchmark directConv(α,0,-1,β,:ZeroPadding,:ZeroPadding);"
+},
+
+{
+    "location": "index.html#α_offset-parameter-1",
+    "page": "Home",
+    "title": "α_offset parameter",
+    "category": "section",
+    "text": "Let's start with a basic example, with zero padding boundary extensions. This example shows the role of the α_offset parameter.using DirectConvolution\nα=Float64[0,1,0];\nβ=collect(Float64,1:6);\nγ1=directConv(α,0,-1,β,:ZeroPadding,:ZeroPadding); # α_offset = 0\nγ2=directConv(α,1,-1,β,:ZeroPadding,:ZeroPadding); # α_offset = 1\nprintln(\"Filter coefficients\") # hide\nprintln(\"α = $(α)\") # hide\nprintln(\"Computation with α_offset=0 (observe the signal shift)\") # hide\nprintln(\"β  = $(β)\") # hide\nprintln(\"γ1 = $(γ1)\") # hide\nprintln(\"Computation with α_offset=1 (observe the phase is corrected)\") # hide\nprintln(\"β  = $(β)\") # hide\nprintln(\"γ2 = $(γ2)\") # hideCAVEAT: under construction"
 },
 
 {
@@ -45,7 +53,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Home",
     "title": "DirectConvolution.directConv",
     "category": "Method",
-    "text": "directConv(tilde_α::AbstractArray{T,1},\n            α_offset::Int64,\n            λ::Int64,\n\n            β::AbstractArray{T,1},\n\n            LeftBoundary::Symbol,\n            RightBoundary::Symbol)\n\nCompute convolution.\n\nHere's some inline maths: sqrtn1 + x + x^2 + ldots.\n\nHere's an equation:\n\nfracnk(n - k) = binomnk\n\nThis is the binomial coefficient.\n\nReturn γ, a created vector of length identical to β one.\n\n\n\n"
+    "text": "directConv(tilde_α::AbstractArray{T,1},\n            α_offset::Int64,\n            λ::Int64,\n\n            β::AbstractArray{T,1},\n\n            LeftBoundary::Symbol,\n            RightBoundary::Symbol)\n\nCompute convolution.\n\nReturn γ, a created vector of length identical to β one.\n\n\n\n"
 },
 
 {
