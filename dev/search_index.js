c412dc1f2845f183ffcceb199338fb62fbc9f7ae
Documenter.jl <documenter@juliadocs.github.io>
2021-08-31 09:42:19 +0000

    build based on 26032c1

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = DirectConvolution","category":"page"},{"location":"#DirectConvolution","page":"Home","title":"DirectConvolution","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for DirectConvolution.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [DirectConvolution]","category":"page"}]
+}
