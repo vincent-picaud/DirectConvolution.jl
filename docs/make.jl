26032c10027b43cd14748c1a6f2e0d6950bdefb0
vincent-picaud <picaud.vincent@gmail.com>
2021-08-31 11:28:15 +0200

    migration update docs

---
+++ docs/make.jl
@@ -0,0 +1,23 @@
+using DirectConvolution
+using Documenter
+
+DocMeta.setdocmeta!(DirectConvolution, :DocTestSetup, :(using DirectConvolution); recursive=true)
+
+makedocs(;
+    modules=[DirectConvolution],
+    authors="vincent-picaud <picaud.vincent@gmail.com> and contributors",
+    repo="https://github.com/vincent-picaud/DirectConvolution.jl/blob/{commit}{path}#{line}",
+    sitename="DirectConvolution.jl",
+    format=Documenter.HTML(;
+        prettyurls=get(ENV, "CI", "false") == "true",
+        canonical="https://vincent-picaud.github.io/DirectConvolution.jl",
+        assets=String[],
+    ),
+    pages=[
+        "Home" => "index.md",
+    ],
+)
+
+deploydocs(;
+    repo="github.com/vincent-picaud/DirectConvolution.jl",
+)
---
+++ docs/src/index.md
@@ -0,0 +1,14 @@
+```@meta
+CurrentModule = DirectConvolution
+```
+
+# DirectConvolution
+
+Documentation for [DirectConvolution](https://github.com/vincent-picaud/DirectConvolution.jl).
+
+```@index
+```
+
+```@autodocs
+Modules = [DirectConvolution]
+```
